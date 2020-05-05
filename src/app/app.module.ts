ba60ddccefe0ff1a0d50844f9f140c2f4c6fff69
nanileb <47608919+nanileb@users.noreply.github.com>
2020-05-05 13:51:54 +0200

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ScanComponent } from './scan/scan.component';
+import { RouterModule, Routes } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ArticleComponent } from './article/article.component';
+import { ListArticleComponent } from './list-article/list-article.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'listArticle', component: ListArticleComponent },
+  { path: 'article', component: ArticleComponent },
+  { path: 'scan', component: ScanComponent },
+  { path: '', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScanComponent,
+    ArticleComponent,
+    ListArticleComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
